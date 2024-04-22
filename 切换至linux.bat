96a19604f7e771fb60c2dae5df25497d81b09866
awsl1414 <3030994569@qq.com>
2024-04-22 16:32:11 +0800

    update

--- 切换至linux.bat
+++ 切换至linux.bat
@@ -1,8 +1,5 @@
 @echo off
-diskpart /s "%~dp0diskpart_cmd1.txt"
-rmdir /S /Q Z:\EFI
-xcopy C:\Users\awsl1414\Desktop\EFI Z:\EFI /E /H /K /I
-diskpart /s "%~dp0diskpart_cmd2.txt"
 chcp 65001
-echo 执行完成，请重启电脑
+xcopy ".\grub.cfg" "F:\@\boot\grub" /K /Y
+echo 请重启电脑
 pause
